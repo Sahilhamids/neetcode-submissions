862209dd7c8a35b6d4c2df163cc1d52927812cfe
SAHIL HAMID SHAIKH <ssahil9635@gmail.com>
2026-06-23 12:16:46 +0530

    Add: first-missing-positive - submission-3

---
+++ Data Structures & Algorithms/first-missing-positive/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n=len(nums)
+        
+        # Rearrange elements to to position as nums[i] should be at position index nums[i]-1
+        # Using elements as key 
+        for i in range(n):
+
+            #olny swap when the number is in range 1 to n and is not already in correct position
+            while 1<=nums[i]<=n and nums[nums[i]-1] != nums[i]:
+                val = nums[i]    
+                nums[i],nums[val-1]=nums[val-1],nums[i]
+                
+        # 2nd pass to check if position is correct 
+        for i in range(n):
+        
+            if nums[i] == i+1:
+                continue
+            else:
+                #element missing and the index at which correct element is missing is the answer
+                return int(i+1)
+        # If all numbers from 1 to n are present then the missing number is n+1
+        return n+1
